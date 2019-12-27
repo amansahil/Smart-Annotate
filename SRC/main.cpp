79fa6861e6673380a491632250b251673bdd4a0b
Aman Sahil <aman.salam2@gmail.com>
2019-12-27 05:37:34 +0000

    Added scroll bar

--- SRC/main.cpp
+++ SRC/main.cpp
@@ -1,11 +1,16 @@
 #include "labeller.h"
 
 #include <QApplication>
+#include <QScrollArea>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Labeller w;
-    w.show();
+
+    QScrollArea area;
+    area.setWidget(&w);
+    area.show();
+
     return a.exec();
 }
